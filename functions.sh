27e679ac185ce11769e5daf4de91914200444e4d
Yash-Garg <yash.garg@outlook.in>
2021-07-03 21:57:20 +0530

    Add if empty input check on clone
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- functions.sh
+++ functions.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
 function mkcd {
-    mkdir -p $1 && cd $1
+    if [ -z ${1} ]; then
+        echo "Invalid Input - Supply a folder name"
+        return
+    else
+        mkdir -p $1 && cd $1
+    fi
 }
 
 function ghclone {
-    git clone git@github.com:$1
+    if [ -z ${1} ]; then
+        echo "Invalid Input - Supply a repository name dumb"
+        return
+    else
+        git clone git@github.com:$1
+    fi
 }
