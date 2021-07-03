be0381086e761034ad5618af36adba3d67f6f17b
Yash-Garg <yash.garg@outlook.in>
2021-07-03 22:10:31 +0530

    Add weather and reload function
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- functions.sh
+++ functions.sh
@@ -17,3 +17,15 @@ function ghclone {
         git clone git@github.com:$1
     fi
 }
+
+function reload {
+    source ~/.zshrc
+}
+
+function weather {
+    if [ "$(tput cols)" -lt 125 ]; then # 125 is min size for correct display
+        curl "wttr.in/~${1:-Ghaziabad}?0"
+    else
+        curl "wttr.in/~${1:-Ghaziabad}"
+    fi
+}
