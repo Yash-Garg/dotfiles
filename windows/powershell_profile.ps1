b34398f78e1d64d0ed5a87510a367d7eec27e737
Yash-Garg <yash.garg@outlook.in>
2021-07-19 14:35:08 +0530

    Add some if else conditions to pwsh functions
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- windows/powershell_profile.ps1
+++ windows/powershell_profile.ps1
@@ -17,11 +17,19 @@ function reload {
 
 # Create a signed commit with given message
 function commit ([string] $commitMessage) {
-    git commit --all -S -s -am $commitMessage
+    if ($commitMessage -ne "") {
+        git commit --all -S -s -am $commitMessage
+    } else {
+        Write-Host "Aborting! No commit message provided."
+    }
 }
 
 # Show oneline log of commits of given size
 function lg ([int] $logSize) {
+    if ($logSize -le 0) {
+        $logSize = 10
+        Write-Host "Using default log size of $logSize"
+    }
     git log --oneline -n $logSize
 }
 
@@ -30,6 +38,9 @@ function ls { Get-Item * }
 
 # Push changes to github repository
 function push { git push }
+
+# Force Push changes to github repository
+function fpush { git push -f }
 
 # Show the status of files in the current working directory
 function st { git status }
