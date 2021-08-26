48c2960f48ad989caad734c22e341434b44cd2f6
Yash-Garg <yash.garg@outlook.in>
2021-08-26 16:43:15 +0530

    shell: add windscribe installation steps
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- setup.sh
+++ setup.sh
@@ -11,12 +11,16 @@ cd ~/dotfiles || exit
 # Setup custom packages key & repos
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add -
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo add-apt-repository ppa:tatokis/ckb-next -y
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
+echo 'deb https://repo.windscribe.com/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo rm -f packages.microsoft.gpg
+
+sudo add-apt-repository ppa:tatokis/ckb-next -y
 
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 
@@ -28,7 +32,7 @@ sudo apt-get -y upgrade
 sudo apt install -y zsh git htop neofetch aria2 curl \
     zip unzip p7zip-full speedtest imwheel pavucontrol \
     pkg-config make cmake scrcpy xclip tailscale ckb-next \
-    telegram-desktop clang code apt-transport-https
+    telegram-desktop clang code apt-transport-https windscribe-cli
 
 # Setup Flutter
 git clone https://github.com/flutter/flutter -b stable ~/Android/flutter
