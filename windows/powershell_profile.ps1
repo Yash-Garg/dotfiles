43be2de6539360febecc79fa9d492d7f3143efc5
Yash Garg <yash.garg@outlook.in>
2023-04-13 21:18:45 +0530

    [pwsh] update fzf settings

--- windows/powershell_profile.ps1
+++ windows/powershell_profile.ps1
@@ -1,5 +1,11 @@
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\starship.toml"
+$ENV:FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix'
+$ENV:FZF_DEFAULT_OPTS=@"
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+"@
 
 # Aliases
 Set-Alias neofetch winfetch.ps1
