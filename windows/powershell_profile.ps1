feca838b64531a446120bb5d8970abac4db58b2d
Yash Garg <yash.garg@outlook.in>
2023-04-14 17:52:17 +0530

    [pwsh] update again

--- windows/powershell_profile.ps1
+++ windows/powershell_profile.ps1
@@ -2,8 +2,8 @@ Invoke-Expression (&starship init powershell)
 
 # Environment variables
 $ENV:STARSHIP_CONFIG = "$HOME\starship.toml"
-$ENV:FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
-$ENV:FZF_DEFAULT_OPTS=@"
+$ENV:FZF_DEFAULT_COMMAND = 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+$ENV:FZF_DEFAULT_OPTS = @"
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
@@ -14,13 +14,19 @@ Set-Alias neofetch winfetch.ps1
 Set-Alias ls lsd
 Set-Alias a2 aria2c
 
+# To find where is the given command called from
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
 # Create a new empty file in the current working directory
-function touch ([string] $fileName) {
-    if (!(Test-Path $fileName)) {
-        Write-Host "Creating $fileName"
-        New-Item -Path . -Name $fileName -ItemType File
-    } else {
-        Write-Host "File '$fileName' already exists"
+function touch ([string] $file) {
+    if (!(Test-Path $file)) {
+        Write-Host "Creating $file"
+        "" | Out-File $file -Encoding ASCII
+    }
+    else {
+        Write-Host "File '$file' already exists"
     }
 }
 
@@ -29,7 +35,8 @@ function mkdir ([string] $dirName) {
     if (!(Test-Path $dirName)) {
         Write-Host "Creating $dirName"
         New-Item -Path . -Name $dirName -ItemType Directory
-    } else {
+    }
+    else {
         Write-Host "Directory '$dirName' already exists"
     }
 }
@@ -38,7 +45,8 @@ function mkdir ([string] $dirName) {
 function commit ([string] $commitMessage) {
     if ($commitMessage -ne "") {
         git commit -S -m $commitMessage
-    } else {
+    }
+    else {
         git commit -S @args
     }
 }
@@ -48,7 +56,8 @@ function lg ([int] $logSize) {
     if ($logSize -le 0) {
         Write-Host "Using default log size of 10"
         git log --oneline --decorate --graph --all -n 10
-    } else {
+    }
+    else {
         git log --oneline --decorate --graph --all -n $logSize
     }
 }
@@ -57,18 +66,10 @@ function lg ([int] $logSize) {
 function rebase ([int] $num) {
     if ($num -eq 0) {
         Write-Host "How many number of commits?"
-    } else {
+    }
+    else {
         git rebase -i HEAD~$num
     }
-}
-
-# Move up the given number of directories
-function up ([int] $count) {
-    if ($count -eq " ") {
-        $count = 1
-    }
-
-    foreach ($i in 1..$count) { cd .. }
 }
 
 # Open fzf and show the selected file in bat
@@ -104,7 +105,7 @@ function fpush { git push --force @args }
 function st { git status @args }
 
 # Open commands history log in notepad
-function cmds { notepad (Get-PSReadLineOption | select -ExpandProperty HistorySavePath) }
+function cmds { notepad (Get-PSReadLineOption | Select-Object -ExpandProperty HistorySavePath) }
 
 # Cherry pick the given commit
 function gcp { git cherry-pick @args }
@@ -113,7 +114,7 @@ function gcp { git cherry-pick @args }
 function dtags { git tag | foreach-object -process { git tag -d $_ } }
 
 # Delete all local branches in the current repository
-function dbranch { git for-each-ref --format '%(refname:short)' refs/heads | ForEach-Object {git branch $_ -D}  }
+function dbranch { git for-each-ref --format '%(refname:short)' refs/heads | ForEach-Object { git branch $_ -D } }
 
 # Diff between HEAD and current changes in the current repository
 function gdiff { git diff HEAD @args | bat --pager=never }
@@ -131,4 +132,4 @@ function sha1 { Get-FileHash -Algorithm SHA1 $args }
 
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
-cls
+Clear-Host
