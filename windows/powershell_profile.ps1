beed7cca302560ebdac2f44bb554ed4115f996e4
Yash-Garg <yash.garg@outlook.in>
2023-05-15 20:17:55 +0530

    win: hide line numbers in diff and remove fzf bg

--- windows/powershell_profile.ps1
+++ windows/powershell_profile.ps1
@@ -17,7 +17,7 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 $ENV:STARSHIP_CONFIG = "$HOME\starship.toml"
 $ENV:FZF_DEFAULT_COMMAND = 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
 $ENV:FZF_DEFAULT_OPTS = @"
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+--color=bg+:#313244,bg:-1,spinner:#f5e0dc,hl:#f38ba8
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
 "@
