0511f65472b2987ff2a379be3c204877972489a0
Yash-Garg <yash.garg@outlook.in>
2023-05-30 00:38:03 +0530

    nix: add cmb function

--- functions.sh
+++ functions.sh
@@ -25,3 +25,18 @@ function gdiff() {
 
     git diff -w $sha
 }
+
+function cmb() {
+    if [ -d .git ]; then
+        git log --graph --color=always \
+            --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+            fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+                --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show -w --color=always % | less -R') << 'FZF-EOF'
+                {}
+        FZF-EOF"
+    else
+        echo "not a git directory"
+    fi
+}
