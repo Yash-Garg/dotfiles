c55f1692b9a68a755cbe837b7eb95ba9f66ea5e8
Yash-Garg <yash.garg@outlook.in>
2023-06-21 23:54:21 +0530

    nix/wsl: update home.nix version and substituters

--- nixos/wsl-configuration.nix
+++ nixos/wsl-configuration.nix
@@ -21,7 +21,7 @@
   nix = {
     package = pkgs.nixFlakes;
     settings = {
-      substituters = [
+      trusted-substituters = [
         "https://devenv.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
@@ -189,5 +189,5 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.05";
 }
