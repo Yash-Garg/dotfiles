81853e73604207611dcf7575b2aef2c7eead44e7
Yash-Garg <yash.garg@outlook.in>
2023-09-09 23:18:23 +0530

    nix: use numtide devshell for rust

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager Configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- nixos/shell-configs/rust/flake.nix
+++ nixos/shell-configs/rust/flake.nix
@@ -1,37 +1,80 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
+    crane,
+    devshell,
+    fenix,
     flake-utils,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        overlays = [rust-overlay.overlays.default];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
-      in
-        with pkgs; {
-          devShells.default = mkShell {
-            buildInputs = [
-              libressl
-              pkg-config
-              toolchain
-            ];
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [devshell.overlays.default];
+      };
 
-            shellHook = ''
-              rustc --version
-            '';
-          };
-        }
-    );
+      rustStable = (import fenix {inherit pkgs;}).fromToolchainFile {
+        file = ./toolchain.toml;
+        sha256 = "sha256-Q9UgzzvxLi4x9aWUJTn+/5EXekC98ODRU1TwhUs9RnY=";
+      };
+
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;
+
+      custom-package = craneLib.buildPackage {
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
+        buildInputs = [];
+        nativeBuildInputs = [];
+        cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+      };
+    in {
+      checks = {
+        inherit custom-package;
+      };
+
+      packages.default = custom-package;
+
+      apps.default = flake-utils.lib.mkApp {
+        drv = custom-package;
+      };
+
+      devShells.default = pkgs.devshell.mkShell {
+        env = [
+          {
+            name = "DEVSHELL_NO_MOTD";
+            value = 1;
+          }
+        ];
+
+        packages = with pkgs; [
+          gcc
+          rustStable
+          libressl
+        ];
+      };
+    });
 }
