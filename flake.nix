3aa77e74c79a00d7d1cdbcf8eeff005f57202ac9
Yash-Garg <yash.garg@outlook.in>
2024-02-10 23:13:05 +0530

    cleanup(nix): move darwin system in main flake

--- flake.nix
+++ flake.nix
@@ -3,18 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     devshell-rust = {
       url = "github:Yash-Garg/dotfiles/?dir=shell-configs/rust";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = {
+    self,
     nixpkgs,
+    nix-darwin,
     home-manager,
     systems,
     ...
@@ -51,6 +60,24 @@
         ./hosts/user.nix
       ];
     };
+
+    darwinConfigurations.trinity = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+
+      specialArgs = {inherit inputs;};
+
+      modules = [
+        ./darwin
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.users.yash = import ./darwin/home.nix;
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations.trinity.pkgs;
 
     devShells = eachSystem (system: {
       rust = inputs.devshell-rust.devShells.${system}.default;
