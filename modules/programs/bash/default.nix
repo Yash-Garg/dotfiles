7c4dbebaaa56178cd4524db03e7c1d992f47e4e5
Yash-Garg <yash.garg@outlook.in>
2024-02-17 17:52:21 +0530

    chore(nix): move aliases to separate script file

--- modules/programs/bash/default.nix
+++ modules/programs/bash/default.nix
@@ -17,22 +17,10 @@ in {
       historySize = 10000;
       historyFile = "$HOME/.bash_history";
       historyControl = ["ignorespace" "erasedups"];
-      initExtra = "source $HOME/.functions";
-      shellAliases = {
-        cat = "bat";
-        cd = "z";
-        cls = "clear";
-        push = "git push";
-        fpush = "git push --force";
-        add = "git add --all";
-        pull = "git pull --rebase";
-        st = "git status";
-        gcp = "git cherry-pick";
-        amend = "git commit --amend";
-        commit = "git commit --all -m";
-        rst = "git reset; git restore *";
-        hs = "home-manager switch";
-      };
+      initExtra = ''
+        source $HOME/.functions
+        source $HOME/.aliases
+      '';
     };
   };
 }
