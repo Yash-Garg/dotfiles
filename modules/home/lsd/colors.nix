ea81a2c24a918c975b9fcbed591efccb8de5fa73
Yash-Garg <me@yashgarg.dev>
2024-02-29 21:04:55 +0530

    feat(nix): specify colorscheme for lsd

---
+++ modules/home/lsd/colors.nix
@@ -0,0 +1,32 @@
+# https://github.com/hardhackerlabs/theme-lsd/blob/master/hardhacker.yaml
+{
+  user = "cyan";
+  group = "yellow";
+  permission = {
+    read = "green";
+    write = "yellow";
+    exec = "red";
+    exec-sticky = "magenta";
+    no-access = "dark_grey";
+  };
+  date = {
+    hour-old = "grey";
+    day-old = "grey";
+    older = "dark_grey";
+  };
+  size = {
+    none = "green";
+    small = "green";
+    medium = "green";
+    large = "green";
+  };
+  inode = {
+    valid = "blue";
+    invalid = "dark_grey";
+  };
+  links = {
+    valid = "blue";
+    invalid = "dark_grey";
+  };
+  tree-edge = "dark_grey";
+}
--- modules/home/lsd/default.nix
+++ modules/home/lsd/default.nix
@@ -2,7 +2,9 @@ _: {
   programs.lsd = {
     enable = true;
     enableAliases = true;
+    colors = import ./colors.nix;
     settings = {
+      date = "relative";
       icons.when = "never";
     };
   };
