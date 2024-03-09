a76ef1228130618730fc464bad19d1d79493e392
Yash-Garg <me@yashgarg.dev>
2024-03-10 01:23:20 +0530

    nix(darwin): remove pandan from brew

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -13,7 +13,6 @@ _: {
     masApps = {
       "Adguard for Safari" = 1440147259;
       "Amphetamine" = 937984704;
-      "Pandan" = 1569600264;
       "Prime Video" = 545519333;
       "Telegram" = 747648890;
       "WhatsApp" = 310633997;
