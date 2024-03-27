189f8aa470501d9b0139c44821d5a854395e4c76
Yash-Garg <me@yashgarg.dev>
2024-03-27 13:07:38 +0530

    chore(nix): disable up arrow for atuin

--- modules/home/atuin/default.nix
+++ modules/home/atuin/default.nix
@@ -3,6 +3,7 @@
     enable = true;
     enableBashIntegration = !pkgs.stdenv.isDarwin;
     enableZshIntegration = pkgs.stdenv.isDarwin;
+    flags = ["--disable-up-arrow"];
     settings = {
       max_preview_height = 2;
       search_mode = "skim";
