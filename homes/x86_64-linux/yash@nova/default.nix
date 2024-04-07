b79ded1692f3d015edc387ba3f49c94c1755cb19
Yash Garg <me@yashgarg.dev>
2024-04-08 02:09:18 +0530

    chore(nix): cleanup unused systemd services
    Signed-off-by: Yash Garg <me@yashgarg.dev>

--- homes/x86_64-linux/yash@nova/default.nix
+++ homes/x86_64-linux/yash@nova/default.nix
@@ -24,25 +24,5 @@
     xclip
   ];
 
-  systemd.user.services.optimise-nix-store = {
-    Unit = {Description = "nix store maintenance";};
-
-    Service = {
-      CPUSchedulingPolicy = "idle";
-      IOSchedulingClass = "idle";
-      ExecStart = toString (pkgs.writeShellScript "nix-optimise-store" ''
-        ${pkgs.nix}/bin/nix-collect-garbage -d
-        ${pkgs.nix}/bin/nix store gc
-        ${pkgs.nix}/bin/nix store optimise
-      '');
-    };
-  };
-
-  systemd.user.timers.optimise-nix-store = {
-    Unit.Description = "nix store maintenance";
-    Timer.OnCalendar = "weekly";
-    Install.WantedBy = ["timers.target"];
-  };
-
   home.stateVersion = "23.11";
 }
