b62e4454b1eb503e9a64f8eb795161bf15a0e701
Yash-Garg <me@yashgarg.dev>
2024-04-08 11:22:08 +0530

    chore(nix): split guiPkgs to separate variable

--- homes/x86_64-linux/yash@nova/default.nix
+++ homes/x86_64-linux/yash@nova/default.nix
@@ -1,28 +1,10 @@
-{pkgs, ...}: {
+_: {
   snowfallorg.user = {
     enable = true;
     name = "yash";
   };
 
   shells.bash.enable = true;
-
-  home.packages = with pkgs; [
-    apktool
-    discord
-    flutter
-    google-chrome
-    imwheel
-    jdk17
-    jetbrains-toolbox
-    kitty
-    (nerdfonts.override {
-      fonts = ["CascadiaCode" "JetBrainsMono"];
-    })
-    scrcpy
-    telegram-desktop
-    vscode
-    xclip
-  ];
 
   home.stateVersion = "23.11";
 }
--- systems/x86_64-linux/nova/default.nix
+++ systems/x86_64-linux/nova/default.nix
@@ -5,9 +5,17 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  guiPkgs = with pkgs; [
+    ddccontrol
+    discord
+    google-chrome
+    jetbrains-toolbox
+    telegram-desktop
+    vscode
+  ];
+in {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
 
@@ -15,31 +23,13 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nova"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "nova";
 
   # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -53,50 +43,52 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.yash = {
     isNormalUser = true;
     description = "Yash Garg";
     extraGroups = ["networkmanager" "wheel"];
+    packages = with pkgs;
+      [
+        apktool
+        flutter
+        imwheel
+        jdk17
+        (nerdfonts.override {
+          fonts = ["CascadiaCode" "JetBrainsMono"];
+        })
+        scrcpy
+        xclip
+      ]
+      ++ guiPkgs;
   };
 
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "yash";
+  services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+
+    # Enable automatic login for the user.
+    displayManager.autoLogin.enable = true;
+    displayManager.autoLogin.user = "yash";
+
+    # Configure keymap in X11
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
