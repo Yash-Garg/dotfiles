4d279c8fd380e03b1708149878c6c7179a219ebd
Yash-Garg <me@yashgarg.dev>
2024-05-13 14:33:33 +0530

    chore(nix): sort inputs and add a default nix file

---
+++ default.nix
@@ -0,0 +1,73 @@
+let
+  inherit (builtins) currentSystem fromJSON readFile;
+
+  # Copied from https://github.com/edolstra/flake-compat/pull/44/files
+  fetchurl = {
+    url,
+    sha256,
+  }:
+    derivation {
+      builder = "builtin:fetchurl";
+
+      name = "source";
+      inherit url;
+
+      outputHash = sha256;
+      outputHashAlgo = "sha256";
+      outputHashMode = "recursive";
+      executable = false;
+      unpack = false;
+
+      system = "builtin";
+
+      # No need to double the amount of network traffic
+      preferLocalBuild = true;
+
+      impureEnvVars = [
+        # We borrow these environment variables from the caller to allow
+        # easy proxy configuration.  This is impure, but a fixed-output
+        # derivation like fetchurl is allowed to do so since its result is
+        # by definition pure.
+        "http_proxy"
+        "https_proxy"
+        "ftp_proxy"
+        "all_proxy"
+        "no_proxy"
+      ];
+
+      # To make "nix-prefetch-url" work.
+      urls = [url];
+    };
+
+  getFlake = name:
+    with (fromJSON (readFile ./flake.lock)).nodes.${name}.locked; {
+      inherit rev;
+      outPath = fetchTarball {
+        url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+        sha256 = narHash;
+      };
+    };
+
+  getRawFlake = name:
+    with (fromJSON (readFile ./flake.lock)).nodes.${name}.locked; {
+      inherit rev;
+      outPath = fetchurl {
+        inherit url;
+        sha256 = narHash;
+      };
+    };
+in
+  {
+    system ? currentSystem,
+    pkgs ?
+      import (getFlake "nixpkgs") {
+        localSystem = {
+          inherit system;
+        };
+      },
+    fenix ? getFlake "fenix",
+    rust-manifest ? getRawFlake "rust-manifest",
+  }: let
+    callPackage = pkg: pkgs.callPackage pkg;
+  in {
+  }
--- flake.nix
+++ flake.nix
@@ -2,11 +2,6 @@
   description = "NixOS and Home Manager Configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     colors-micro.url = "https://raw.githubusercontent.com/Yash-Garg/dotfiles/stable/modules/home/micro/colors.micro";
     colors-micro.flake = false;
 
@@ -27,8 +22,13 @@
     flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
     flake-utils-plus.inputs.flake-utils.follows = "flake-utils";
 
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     nix-topology.url = "github:oddlama/nix-topology";
     nix-topology.inputs.nixpkgs.follows = "nixpkgs";
