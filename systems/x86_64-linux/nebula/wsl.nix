af769f8ee9949d52ee316ba1ccf60c5c0dd36c47
Yash-Garg <me@yashgarg.dev>
2024-07-07 19:07:02 +0530

    refactor: move wsl specific stuff to separate file

---
+++ systems/x86_64-linux/nebula/wsl.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  wsl = {
+    enable = true;
+    defaultUser = "yash";
+    nativeSystemd = true;
+    startMenuLaunchers = true;
+    usbip.enable = true;
+    wslConf.network.hostname = "nebula";
+
+    # Binaries for Docker Desktop wsl-distro-proxy
+    extraBin = with pkgs; [
+      {src = "${coreutils}/bin/mkdir";}
+      {src = "${coreutils}/bin/cat";}
+      {src = "${coreutils}/bin/whoami";}
+      {src = "${coreutils}/bin/ls";}
+      {src = "${busybox}/bin/addgroup";}
+      {src = "${su}/bin/groupadd";}
+      {src = "${su}/bin/usermod";}
+    ];
+  };
+}
