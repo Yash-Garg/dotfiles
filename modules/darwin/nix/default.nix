82e0f06131ac606dda6677a31156148cb9c4a651
Yash-Garg <me@yashgarg.dev>
2024-07-07 13:20:35 +0530

    refactor: commonize nix config

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -1,30 +1,18 @@
-{pkgs, ...}: {
-  nix = {
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 3d";
+{
+  lib,
+  pkgs,
+  inputs,
+  namespace,
+  ...
+}: {
+  nix =
+    lib.${namespace}.mkNixConfig {inherit lib pkgs inputs;}
+    // {
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 3d";
+      };
     };
-
-    package = pkgs.nixVersions.git;
-
-    settings = {
-      trusted-substituters = [
-        "https://cache.garnix.io"
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org"
-      ];
-      trusted-public-keys = [
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      ];
-      trusted-users = ["yash" "root"];
-    };
-    extraOptions = ''
-      ssl-cert-file = /private/etc/ssl/cert.pem
-      extra-nix-path = nixpkgs=flake:nixpkgs
-    '';
-  };
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -1,8 +1,8 @@
 {
   lib,
   pkgs,
-  config,
   inputs,
+  namespace,
   ...
 }: {
   documentation = {
@@ -12,60 +12,19 @@
     dev.enable = false;
   };
 
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 3d";
-      persistent = true;
+  nix =
+    lib.${namespace}.mkNixConfig {inherit lib pkgs inputs;}
+    // {
+      gc = {
+        automatic = true;
+        dates = "daily";
+        options = "--delete-older-than 3d";
+        persistent = true;
+      };
+
+      optimise.automatic = true;
+
+      settings.allowed-users = ["@wheel"];
+      settings.trusted-users = ["root" "@wheel"];
     };
-    optimise.automatic = true;
-
-    package = pkgs.nixVersions.git;
-
-    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
-
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
-
-    extraOptions = ''
-      keep-outputs = true
-      warn-dirty = false
-      keep-derivations = true
-    '';
-    settings = {
-      accept-flake-config = true;
-      allowed-users = ["@wheel"];
-      auto-optimise-store = true;
-      builders-use-substitutes = true;
-      experimental-features = [
-        "auto-allocate-uids"
-        "ca-derivations"
-        "cgroups"
-        "flakes"
-        "nix-command"
-        "recursive-nix"
-      ];
-      flake-registry = "/etc/nix/registry.json";
-      http-connections = 50;
-      keep-going = true;
-      log-lines = 20;
-      max-jobs = "auto";
-      sandbox = true;
-      trusted-users = ["root" "@wheel"];
-      use-cgroups = true;
-      warn-dirty = false;
-
-      trusted-substituters = [
-        "https://cache.garnix.io"
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      ];
-    };
-  };
 }
