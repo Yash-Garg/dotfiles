453b5b67dfca6d0b8f76e2c8b07a58e7907ceabb
Yash-Garg <me@yashgarg.dev>
2024-07-07 18:35:04 +0530

    chore(wsl): add binaries for docker desktop proxy

--- systems/x86_64-linux/nebula/default.nix
+++ systems/x86_64-linux/nebula/default.nix
@@ -10,6 +10,17 @@
     startMenuLaunchers = true;
     usbip.enable = true;
     wslConf.network.hostname = "nebula";
+
+    # Binaries for Docker Desktop wsl-distro-proxy
+    extraBin = with pkgs; [
+      {src = "${coreutils}/bin/mkdir";}
+      {src = "${coreutils}/bin/cat";}
+      {src = "${coreutils}/bin/whoami";}
+      {src = "${coreutils}/bin/ls";}
+      {src = "${busybox}/bin/addgroup";}
+      {src = "${su}/bin/groupadd";}
+      {src = "${su}/bin/usermod";}
+    ];
   };
 
   topology.self.name = "WSL";
