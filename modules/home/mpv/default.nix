45d309be49dc59b8e7758e2e351e94e9da173d4f
Yash-Garg <me@yashgarg.dev>
2024-07-09 02:09:13 +0530

    feat(mpv): add more scripts and opts

--- modules/home/mpv/default.nix
+++ modules/home/mpv/default.nix
@@ -17,9 +17,23 @@ in {
       bindings = import ./bindings.nix;
       config = import ./config.nix;
       scripts = with pkgs.mpvScripts; [
+        acompressor
+        autocrop
+        autoload
         mpv-playlistmanager
+        reload
         sponsorblock
       ];
+      scriptOpts = {
+        osc = {
+          deadzonesize = 0.75;
+          scalewindowed = 0.85;
+          scalefullscreen = 0.95;
+          scaleforcedwindow = 1.5;
+          boxmaxchars = 140;
+          boxalpha = 100;
+        };
+      };
     };
   };
 }
