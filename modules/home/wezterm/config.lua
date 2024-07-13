516fb91146649ed05a0a8be8559d1e303ee5afc4
Yash-Garg <me@yashgarg.dev>
2024-07-13 23:09:12 +0530

    chore: more wezterm config

--- modules/home/wezterm/config.lua
+++ modules/home/wezterm/config.lua
@@ -1,13 +1,15 @@
 local wezterm = require("wezterm");
 local config = wezterm.config_builder();
+config.automatically_reload_config = true;
+config.check_for_updates = false;
 config.color_scheme = "Aura (Gogh)";
 config.enable_scroll_bar = true;
 config.enable_tab_bar = false;
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono");
-config.font_antialias = "Subpixel";
 config.font_size = 15;
-config.initial_cols = 140;
-config.initial_rows = 45;
+config.initial_cols = 130;
+config.initial_rows = 35;
 config.scrollback_lines = 5000;
 config.use_fancy_tab_bar = true;
+config.window_close_confirmation = "NeverPrompt";
 return config;
