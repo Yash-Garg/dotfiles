65a356e55bdcdb1f1403c663981bed23b452b967
Yash-Garg <me@yashgarg.dev>
2024-07-17 04:05:38 +0530

    refactor: move inputs at bottom of flake

--- flake.nix
+++ flake.nix
@@ -1,69 +1,6 @@
 {
   description = "NixOS and Home Manager Configurations";
 
-  inputs = {
-    catppuccin-starship.url = "github:catppuccin/starship";
-    catppuccin-starship.flake = false;
-
-    darwin.url = "github:LnL7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.flake-compat.follows = "flake-compat";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
-    deploy-rs.inputs.utils.follows = "flake-utils";
-
-    devshell.url = "github:numtide/devshell";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
-    devshell.inputs.flake-utils.follows = "flake-utils";
-
-    flake-compat.url = "github:nix-community/flake-compat";
-    flake-compat.flake = false;
-
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
-    flake-utils-plus.inputs.flake-utils.follows = "flake-utils";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    lix.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
-    lix.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    nix-topology.url = "github:oddlama/nix-topology";
-    nix-topology.inputs.nixpkgs.follows = "nixpkgs";
-    nix-topology.inputs.devshell.follows = "devshell";
-    nix-topology.inputs.flake-utils.follows = "flake-utils";
-
-    nixos-wsl.url = "github:getchoo/NixOS-WSL/hardware-graphics";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-wsl.inputs.flake-compat.follows = "flake-compat";
-    nixos-wsl.inputs.flake-utils.follows = "flake-utils";
-
-    nur.url = "github:nix-community/NUR";
-
-    snowfall-lib.url = "github:snowfallorg/lib/dev";
-    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
-    snowfall-lib.inputs.flake-utils-plus.follows = "flake-utils-plus";
-
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
-    spicetify-nix.inputs.flake-compat.follows = "flake-compat";
-
-    srvos.url = "github:nix-community/srvos";
-    srvos.inputs.nixpkgs.follows = "nixpkgs";
-
-    stylix.url = "github:danth/stylix";
-    stylix.inputs.flake-compat.follows = "flake-compat";
-    stylix.inputs.home-manager.follows = "home-manager";
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
   outputs =
     inputs:
     let
@@ -164,4 +101,67 @@
     // {
       self = inputs.self;
     };
+
+  inputs = {
+    catppuccin-starship.url = "github:catppuccin/starship";
+    catppuccin-starship.flake = false;
+
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.flake-compat.follows = "flake-compat";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
+    deploy-rs.inputs.utils.follows = "flake-utils";
+
+    devshell.url = "github:numtide/devshell";
+    devshell.inputs.nixpkgs.follows = "nixpkgs";
+    devshell.inputs.flake-utils.follows = "flake-utils";
+
+    flake-compat.url = "github:nix-community/flake-compat";
+    flake-compat.flake = false;
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    flake-utils-plus.inputs.flake-utils.follows = "flake-utils";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    lix.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+    lix.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    nix-topology.url = "github:oddlama/nix-topology";
+    nix-topology.inputs.nixpkgs.follows = "nixpkgs";
+    nix-topology.inputs.devshell.follows = "devshell";
+    nix-topology.inputs.flake-utils.follows = "flake-utils";
+
+    nixos-wsl.url = "github:getchoo/NixOS-WSL/hardware-graphics";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-wsl.inputs.flake-compat.follows = "flake-compat";
+    nixos-wsl.inputs.flake-utils.follows = "flake-utils";
+
+    nur.url = "github:nix-community/NUR";
+
+    snowfall-lib.url = "github:snowfallorg/lib/dev";
+    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
+    snowfall-lib.inputs.flake-utils-plus.follows = "flake-utils-plus";
+
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
+    spicetify-nix.inputs.flake-compat.follows = "flake-compat";
+
+    srvos.url = "github:nix-community/srvos";
+    srvos.inputs.nixpkgs.follows = "nixpkgs";
+
+    stylix.url = "github:danth/stylix";
+    stylix.inputs.flake-compat.follows = "flake-compat";
+    stylix.inputs.home-manager.follows = "home-manager";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 }
