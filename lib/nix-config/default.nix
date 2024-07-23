771d36347bdc5ee2ff5c63cdff41ec224c26ac78
Yash-Garg <me@yashgarg.dev>
2024-07-23 10:26:27 +0530

    chore: add my personal cachix cache

--- lib/nix-config/default.nix
+++ lib/nix-config/default.nix
@@ -34,15 +34,13 @@
         warn-dirty = false;
 
         trusted-substituters = [
-          "https://cache.garnix.io"
-          "https://nix-community.cachix.org"
+          "https://yash-garg.cachix.org"
           "https://cache.nixos.org"
           "https://raspberry-pi-nix.cachix.org"
         ];
 
         trusted-public-keys = [
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "yash-garg.cachix.org-1:sHcKOvVej+RlINvt4XVAOE/Cnho3hnrHHRv0uq1u7Xs="
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
         ];
