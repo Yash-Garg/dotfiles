f9d667feec6664b9f04d46aa4e0b5a9405bb61fa
Yash-Garg <me@yashgarg.dev>
2024-08-02 22:16:45 +0530

    darwin: add default system preferences

---
+++ modules/darwin/system/default.nix
@@ -0,0 +1,44 @@
+_: {
+  system = {
+    defaults = {
+      dock = {
+        autohide = false;
+        largesize = 110;
+        magnification = true;
+        mineffect = "scale";
+        minimize-to-application = false;
+        orientation = "bottom";
+        show-recents = false;
+        tilesize = 35;
+      };
+
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "Nlsv";
+        QuitMenuItem = false;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
+
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        InitialKeyRepeat = 15;
+        KeyRepeat = 1;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+      };
+
+      trackpad = {
+        Clicking = true;
+      };
+    };
+
+    startup.chime = true;
+  };
+}
