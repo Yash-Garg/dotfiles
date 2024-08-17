e0fd036316593397f4247aab7c4c554ca949054b
Yash-Garg <me@yashgarg.dev>
2024-08-17 10:31:14 +0530

    neovim: use startify theme with modified header

--- modules/home/neovim/plugins/alpha.nix
+++ modules/home/neovim/plugins/alpha.nix
@@ -2,152 +2,24 @@
 with lib.${namespace};
 {
   programs.nixvim.plugins.alpha = enabled // {
-    theme = null;
     iconsEnabled = true;
-    layout =
-      let
-        padding = val: {
-          type = "padding";
-          inherit val;
-        };
-      in
-      [
-        (padding 4)
-        {
-          opts = {
-            hl = "AlphaHeader";
-            position = "center";
-          };
-          type = "text";
-          val = [
-            "  ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗  "
-            "  ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║  "
-            "  ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║  "
-            "  ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║  "
-            "  ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║  "
-            "  ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝  "
-          ];
+    theme.__raw = ''
+      (function()
+        local dashboard = require("alpha.themes.startify")
+        dashboard.section.header.val = {
+          [[                                                                       ]],
+          [[                                                                     ]],
+          [[       ████ ██████           █████      ██                     ]],
+          [[      ███████████             █████                             ]],
+          [[      █████████ ███████████████████ ███   ███████████   ]],
+          [[     █████████  ███    █████████████ █████ ██████████████   ]],
+          [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+          [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+          [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+          [[                                                                       ]],
         }
-        (padding 2)
-        {
-          type = "button";
-          val = "  Find File";
-          on_press.raw = "require('telescope.builtin').find_files";
-          opts = {
-            keymap = [
-              "n"
-              "f"
-              ":Telescope find_files <CR>"
-              {
-                noremap = true;
-                silent = true;
-                nowait = true;
-              }
-            ];
-            shortcut = "f";
-            position = "center";
-            cursor = 3;
-            width = 38;
-            align_shortcut = "right";
-            hl_shortcut = "Keyword";
-          };
-        }
-        (padding 1)
-        {
-          type = "button";
-          val = "  New File";
-          on_press.__raw = "function() vim.cmd[[ene]] end";
-          opts = {
-            keymap = [
-              "n"
-              "n"
-              ":ene <BAR> startinsert <CR>"
-              {
-                noremap = true;
-                silent = true;
-                nowait = true;
-              }
-            ];
-            shortcut = "n";
-            position = "center";
-            cursor = 3;
-            width = 38;
-            align_shortcut = "right";
-            hl_shortcut = "Keyword";
-          };
-        }
-        (padding 1)
-        {
-          type = "button";
-          val = "󰈚  Recent Files";
-          on_press.raw = "require('telescope.builtin').oldfiles";
-          opts = {
-            keymap = [
-              "n"
-              "r"
-              ":Telescope oldfiles <CR>"
-              {
-                noremap = true;
-                silent = true;
-                nowait = true;
-              }
-            ];
-            shortcut = "r";
-            position = "center";
-            cursor = 3;
-            width = 38;
-            align_shortcut = "right";
-            hl_shortcut = "Keyword";
-          };
-        }
-        (padding 1)
-        {
-          type = "button";
-          val = "󰈭  Find Word";
-          on_press.raw = "require('telescope.builtin').live_grep";
-          opts = {
-            keymap = [
-              "n"
-              "g"
-              ":Telescope live_grep <CR>"
-              {
-                noremap = true;
-                silent = true;
-                nowait = true;
-              }
-            ];
-            shortcut = "g";
-            position = "center";
-            cursor = 3;
-            width = 38;
-            align_shortcut = "right";
-            hl_shortcut = "Keyword";
-          };
-        }
-        (padding 1)
-        {
-          type = "button";
-          val = "  Quit Neovim";
-          on_press.__raw = "function() vim.cmd[[qa]] end";
-          opts = {
-            keymap = [
-              "n"
-              "q"
-              ":qa<CR>"
-              {
-                noremap = true;
-                silent = true;
-                nowait = true;
-              }
-            ];
-            shortcut = "q";
-            position = "center";
-            cursor = 3;
-            width = 38;
-            align_shortcut = "right";
-            hl_shortcut = "Keyword";
-          };
-        }
-      ];
+        return dashboard.config
+      end)()
+    '';
   };
 }
