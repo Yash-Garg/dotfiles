b5a39a647de8d888edca8ca600e0651c168b8b52
Yash-Garg <me@yashgarg.dev>
2024-09-02 23:49:30 +0530

    direnv: use only home-manager module

--- modules/home/direnv/default.nix
+++ modules/home/direnv/default.nix
@@ -1,12 +1,11 @@
 {
   config,
   lib,
-  pkgs,
   namespace,
   ...
 }:
 with lib.${namespace};
-lib.mkIf pkgs.stdenv.isDarwin {
+{
   programs.direnv = enabled // {
     enableBashIntegration = config.shells.${namespace}.bash.enable;
     enableZshIntegration = config.shells.${namespace}.zsh.enable;
