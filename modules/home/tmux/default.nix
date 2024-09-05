4aff6d84577459464cafd5e22e7dc362ddb26523
Yash-Garg <me@yashgarg.dev>
2024-09-05 11:13:48 +0530

    tmux: tweak top bar styling

--- modules/home/tmux/default.nix
+++ modules/home/tmux/default.nix
@@ -30,10 +30,13 @@ in
           set -g @catppuccin_window_number_position "right"
 
           set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+
           set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
 
           set -g @catppuccin_status_background "default"
-          set -g @catppuccin_status_modules_right "application session"
+          set -g @catppuccin_status_modules_right "directory session"
           set -g @catppuccin_status_left_separator  " "
           set -g @catppuccin_status_right_separator ""
           set -g @catppuccin_status_fill "icon"
