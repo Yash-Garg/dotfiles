a24f522a6b0e7031fb3fac266b22f1a51cf6435d
Yash-Garg <me@yashgarg.dev>
2024-09-11 13:23:11 +0530

    adguard: specify some more settings

--- modules/nixos/services/adguard/default.nix
+++ modules/nixos/services/adguard/default.nix
@@ -12,13 +12,30 @@ in
 {
   options.${namespace}.services.adguard = {
     enable = mkEnableOption "Adguard Home Server";
+
+    port = mkOption {
+      type = types.int;
+      default = 4000;
+      description = "The port to serve HTTP on";
+    };
   };
 
   config = mkIf cfg.enable {
+    networking.firewall = {
+      allowedTCPPorts = [ 53 ];
+      allowedUDPPorts = [ 53 ];
+    };
+
     services.adguardhome = enabled // {
+      inherit (cfg) port;
+      host = "127.0.0.1";
       mutableSettings = true;
       openFirewall = true;
-      port = 3001;
+      settings = {
+        http = {
+          address = "127.0.0.1:${toString cfg.port}";
+        };
+      };
     };
   };
 }
